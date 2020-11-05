5021073143152b3ecb9466a60ff234eb6eb3ed26
javierpugliese <javipugliese1@gmail.com>
2020-11-05 10:20:17 -0300

    Table Maintenance base

--- src/views/TableMaintenance.vue
+++ src/views/TableMaintenance.vue
@@ -3,22 +3,13 @@
     <!-- Make a Component TODO-->
     <v-row dense>
       <v-col v-for="(item, i) in items" :key="i" cols="auto">
-        <v-card :color="item.color" dark>
-          <div class="d-flex flex-no-wrap justify-space-between">
-            <div>
-              <v-card-title class="headline" v-text="item.title"></v-card-title>
-              <v-card-actions>
-                <v-btn class="ml-2 mt-5" outlined rounded small>
-                  Navegar
-                </v-btn>
-              </v-card-actions>
-            </div>
-
-            <!-- Quitar -->
-
-            <v-avatar class="ma-3" size="125" tile>
-              <v-img :src="item.src"></v-img>
-            </v-avatar>
+        <v-card class="px-5 py-3" color="blue-grey darken-4" dark>
+          <div class="d-flex flex-sm-column">
+            <v-card-actions>
+              <v-btn large text :to="item.link">
+                {{ item.title }}
+              </v-btn>
+            </v-card-actions>
           </div>
         </v-card>
       </v-col>
@@ -41,13 +32,13 @@ export default {
       { title: "Barrios", link: "/barrios" },
       { title: "Localidades", link: "/localidades" },
       { title: "Provincias", link: "/provincias" },
-      { title: "Pantallas", link: "/fabricantes" },
+      { title: "Fabricantes", link: "/fabricantes" },
       { title: "Tipos de Pantalla", link: "/tipos-de-pantalla" },
       { title: "Tipos de Archivo", link: "/tipos-de-archivo" },
       { title: "Tipos de Producto", link: "/tipos-de-producto" },
       { title: "Tipos de Atributo", link: "/tipos-de-atributo" },
       { title: "Atributos", link: "/atributos" },
-      { title: "Categorías de Producto", link: "/categorias-de-producto" }
+      { title: "Categorías", link: "/categorias" }
     ]
   })
 };

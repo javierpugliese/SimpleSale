e6afe7fcdb6c2ad1e5338fb2ed7db3cdcf1367c0
javierpugliese <javipugliese1@gmail.com>
2021-03-16 12:14:44 -0300

    Api url changed, disabled prettier and eslint unused vars rule

--- src/main.js
+++ src/main.js
@@ -6,13 +6,13 @@ import vuetify from "./plugins/vuetify";
 import "roboto-fontface/css/roboto/roboto-fontface.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 import axios from "axios";
-import VueWorker from 'vue-worker';
 import vdr from "./components/vdr_gorkys_modified/components/vue-draggable-resizable.vue";
 import "./components/vdr_gorkys_modified/components/vue-draggable-resizable.css";
 
 Vue.config.productionTip = false;
 
-axios.defaults.baseURL = "https://touch-test-app.azurewebsites.net/";
+//axios.defaults.baseURL = "https://touch-test-app.azurewebsites.net/";
+axios.defaults.baseURL = "http://200.10.96.221:8080/";
 
 axios.interceptors.request.use(
   config => {
@@ -28,7 +28,6 @@ axios.interceptors.request.use(
 
 Vue.prototype.$http = axios;
 
-Vue.use(VueWorker, "$worker");
 Vue.component('vdr', vdr);
 
 new Vue({
